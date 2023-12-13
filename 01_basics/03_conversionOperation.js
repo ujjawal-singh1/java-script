1ed667652bf594f2e76370caf2fd38a59d5baf08
Ujjawal Kumar <124488244+ujjawal-singh1@users.noreply.github.com>
2023-12-13 21:53:49 +0000

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,3 +13,28 @@ console.log(newNumber);
 //true=>1; false=>0;
 //""=>false
 //"Ujjawal"=>true
+//**************************OPERATIONS**************//
+let no=12
+let val=-no
+console.log(val)
+
+let str1="Shree"
+let str2="Radhe"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+console.log(+true);
+
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
