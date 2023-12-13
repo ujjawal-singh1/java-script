4a93cf9b04e30de32d1b0a615205fb37de3e5e4f
Ujjawal Kumar <124488244+ujjawal-singh1@users.noreply.github.com>
2023-12-13 21:35:54 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score="33abc"
+
+console.log(typeof score);
+
+let newNumber=Number(score)
+
+console.log(typeof(newNumber));
+
+console.log(newNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+//""=>false
+//"Ujjawal"=>true
