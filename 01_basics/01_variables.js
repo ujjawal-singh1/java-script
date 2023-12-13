05e9a7f18978e52294433ab53a10756c42be2315
Ujjawal Kumar <124488244+ujjawal-singh1@users.noreply.github.com>
2023-12-13 21:22:28 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountNO=41136597788
+let Name="UjjawalKumar"
+var bankName="Sbi"
+branch="Bhagwanpur"//bad practice
+/* var was used on recent compiler in modern 
+comiler we use let
+and main reason is scope and functionality
+*/
+console.log(branch)
+console.table([accountNO,bankName,Name,branch])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are usingjs,not browser
+
+let name="Ujjawal"
+let age=18
+let isLoggedIN=false
+
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//Symbol =>unique
+
+
+//object
+
+console.log(typeof "Ujjawal");
