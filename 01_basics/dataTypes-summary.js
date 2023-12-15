d5feac3b424cdaabb709e0ebc44a82fdad26682f
Ujjawal Kumar <124488244+ujjawal-singh1@users.noreply.github.com>
2023-12-15 12:09:38 +0000

    datatypeSummary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,32 @@
+//   Primitive
+
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIN=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+const bigNumber=345545447797779n
+
+
+//Refrence Type (Non primitive)
+
+// array,objects,functions
+
+const heros=["RaghavJi","hanuman","Raghunandan"]
+let myObj={
+    name: "Ramchandra ji",
+    age: 17,
+}
+
+const myFunction=function(){
+    console.log("HEllo Bhagwan");
+}
+
+console.log(typeof heros )
