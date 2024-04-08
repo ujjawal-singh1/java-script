f0fdf1ebc22af30831c539a12212e0cdcc5dcb59
Ujjawal Kumar <124488244+ujjawal-singh1@users.noreply.github.com>
2024-04-08 22:32:27 +0000

    object part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,36 @@
+//const tinderUser=new Object
+
+const tinderUser={}
+
+tinderUser.id="abc13"
+tinderUser.name="Sam"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser)
+
+const regularUser={
+    email:"monu@gmail.com",
+    fullName:{
+        userFullName:{
+            firstName:"Ujjawal",
+            lastName:"Kumar",
+        }
+    }
+}
+// console.log(regularUser.fullName);
+
+const ob1={1:"a",2:"b"}
+const ob2={2:"a",4:"b"}
+//const ob3=Object.assign({},ob1,ob2)
+
+//.assign(target,source)
+
+//spread operator
+const ob3={...ob1,...ob2}
+console.log(ob3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('id'));
